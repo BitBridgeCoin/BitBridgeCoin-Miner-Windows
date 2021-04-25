3c25e1b79b9f1b6e3aaab435396d095352f7300a
WorldMoney <hujingkun@gmail.com>
2021-04-25 21:57:31 +0800

    use BitBridgeCoin QT as Server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace bitbridgecoinminer
@@ -8,16 +8,19 @@ namespace bitbridgecoinminer
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             Process p = new Process();
-            p.StartInfo = new ProcessStartInfo("bitbridgecoind.exe");
+            p.StartInfo = new ProcessStartInfo("bitbridgecoin-qt.exe");
+            p.StartInfo.Arguments = "-server";
             p.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory().ToString();
             p.StartInfo.CreateNoWindow = true;
             p.Start();
 
             string output = string.Empty;
+            Console.WriteLine("Start Mining BitBridgeCoin");
             while (true) {
                 Process q = new Process();
                 q.StartInfo = new ProcessStartInfo("bitbridgecoin-cli.exe");
                 q.StartInfo.Arguments = "generate 1";
+                q.StartInfo.ErrorDialog = false;
                 q.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory().ToString();
                 q.StartInfo.UseShellExecute = false;
                 q.StartInfo.RedirectStandardOutput = true;
